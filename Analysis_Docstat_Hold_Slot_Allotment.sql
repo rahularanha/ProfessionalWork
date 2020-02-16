40f10d956f2ed386a43def4f552c21b2fc3065e0
Rahul Aranha <rahularanha@gmail.com>
2020-02-17 02:40:51 +0530

    Update Analysis_Docstat_Hold_Slot_Allotment.sql

--- Analysis_Docstat_Hold_Slot_Allotment.sql
+++ Analysis_Docstat_Hold_Slot_Allotment.sql
@@ -1,3 +1,5 @@
+------- Suggestion for number of slots to provide
+
 --- prefinal
 
 SELECT c.order_id, xyz.case_log_id, case_id, d.name AS doc_name, d.phone AS doc_mobile_number,
@@ -116,3 +118,86 @@ WHERE (xyz.note LIKE 'Hold%' OR xyz.note LIKE 'Rx_prescribed%' OR xyz.note LIKE 
 ) final_result
 ORDER BY 2,3
 ;
+
+	
+	
+	
+	
+------------ Impact analysis of hold orders on slots
+	
+-------Slotting Order Analysis
+
+WITH attempt_data AS (
+	SELECT order_id,id, DATEADD(MIN,330,created_at) AS attempted_at,
+			CASE "action" WHEN 1 THEN 'Moved_to_DP'
+						  WHEN 2 THEN 'Assigned'
+						  WHEN 3 THEN 'Rejected'
+						  WHEN 4 THEN 'Accepted'
+						  WHEN 5 THEN 'On Hold'
+						  WHEN 6 THEN 'Awaiting_Order_Creation'
+						  WHEN 7 THEN 'Order_Created'
+						  WHEN 8 THEN 'Doctor_Reassigned'
+						  WHEN 9 THEN 'Queued'
+						  WHEN 10 THEN 'Resheduled'
+			 END AS dp_action,
+			 CASE WHEN "action" IN (3,4,5,8,10)  
+			      THEN ROW_NUMBER() OVER (PARTITION BY order_id,"action" IN (3,4,5,8,10) ORDER BY id) 
+			 END AS attempt_number
+	FROM pe_pe2_pe2.doctor_program_order_history dpoh
+	--WHERE order_id=9279133
+)
+SELECT order_id, order_placed_at, first_moved_to_dp_at, first_response_at, first_response, issue_flag, doctor_category,
+		case_id, booking_id, booking_created_at, booking_deleted_at, booking_updated_at,
+		slot_id, capacity, booked_capacity, user_profile_id, slot_date, slot_start_time, slot_end_time, 
+		slot_created_at, slot_deleted_at, slot_updated_at,
+		MIN(CASE WHEN attempt_before_first=1 THEN ad_b_dp_action END) AS before_first_attempt,
+		MIN(CASE WHEN attempt_before_first=1 THEN ad_b_attempted_at END) AS before_first_attempted_at,
+		MIN(CASE WHEN attempt_before_last=1 THEN ad_b_dp_action END) AS before_last_attempt,
+		MIN(CASE WHEN attempt_before_last=1 THEN ad_b_attempt_number END) AS before_last_attempt_number,
+		MIN(CASE WHEN attempt_before_last=1 THEN ad_b_attempted_at END) AS before_last_attempt_at,
+		MIN(CASE WHEN attempt_after_first=1 THEN ad_a_dp_action END) AS after_first_attempt,
+		MIN(CASE WHEN attempt_after_first=1 THEN ad_a_attempt_number END) AS after_first_attempt_number,
+		MIN(CASE WHEN attempt_after_first=1 THEN ad_a_attempted_at END) AS after_first_attempt_at,
+		MIN(CASE WHEN attempt_after_last=1 THEN ad_a_dp_action END) AS after_last_attempt,
+		MIN(CASE WHEN attempt_after_last=1 THEN ad_a_attempt_number END) AS after_last_attempt_number,
+		MIN(CASE WHEN attempt_after_last=1 THEN ad_a_attempted_at END) AS after_last_attempt_at
+FROM (
+	SELECT fdpo.order_id, fdpo.order_placed_at, fdpo.doctor_category, fdpo.first_moved_to_dp_at, fdpo.first_response_at, fdpo.first_response, fdpo.issue_flag,
+			RIGHT(b.booking_id,7) AS case_id, b.booking_id, DATEADD(MIN,330,b.created_at) AS booking_created_at, DATEADD(MIN,330,b.deleted_at) AS booking_deleted_at, DATEADD(MIN,330,b.updated_at) AS booking_updated_at,
+			b.slot_id, s.capacity, s.booked_capacity, s.user_profile_id, s."date" AS slot_date, s.start_time AS slot_start_time, s.end_time AS slot_end_time, 
+			DATEADD(MIN,330,s.created_at) AS slot_created_at, DATEADD(MIN,330,s.deleted_at) AS slot_deleted_at, DATEADD(MIN,330,s.updated_at) AS slot_updated_at,
+			ad_b.attempted_at AS ad_b_attempted_at, ad_b.attempt_number AS ad_b_attempt_number, ad_b.dp_action AS ad_b_dp_action,
+			ad_a.attempted_at AS ad_a_attempted_at, ad_a.attempt_number AS ad_a_attempt_number, ad_a.dp_action AS ad_a_dp_action,
+			ROW_NUMBER() OVER (PARTITION BY fdpo.order_id ORDER BY ad_b.attempted_at DESC) attempt_before_last,
+			ROW_NUMBER() OVER (PARTITION BY fdpo.order_id ORDER BY ad_b.attempted_at) attempt_before_first,
+			ROW_NUMBER() OVER (PARTITION BY fdpo.order_id ORDER BY ad_a.attempted_at DESC) attempt_after_last,
+			ROW_NUMBER() OVER (PARTITION BY fdpo.order_id ORDER BY ad_a.attempted_at) attempt_after_first
+	FROM data_model.f_doctor_program_order fdpo
+	LEFT JOIN pe_docstat_91streets_media_technologies."case" c ON fdpo.order_id=c.order_id
+	LEFT JOIN pe_hive_slotting_hive.bookings b ON c.id=RIGHT(b.booking_id,7)
+	LEFT JOIN pe_hive_slotting_hive.slots s ON b.slot_id=s.id AND s.user_profile_id=41 
+	LEFT JOIN attempt_data ad_b ON fdpo.order_id=ad_b.order_id AND ad_b.attempted_at<DATEADD(MIN,330,b.created_at) AND ad_b.attempt_number IS NOT NULL
+	LEFT JOIN attempt_data ad_a ON fdpo.order_id=ad_a.order_id AND ad_a.attempted_at>DATEADD(MIN,330,b.created_at) AND ad_a.attempt_number IS NOT NULL
+	WHERE fdpo.num_of_times_on_hold>0 AND DATE(fdpo.order_placed_at) BETWEEN '2019-06-01' AND '2019-06-30'
+	ORDER BY fdpo.order_placed_at 
+)
+--WHERE attempt_before_last=1 OR attempt_before_first=1 OR attempt_after_last=1 OR attempt_after_first=1
+GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22
+ORDER BY 2
+--LIMIT 100;
+
+
+
+---Slotting Slot Analysis
+
+SELECT s.id AS slot_id, s.capacity AS slot_capacity, s.booked_capacity, 
+		s."date" AS slot_date, s.start_time AS slot_start_time, s.end_time AS slot_end_time, 
+		(s."date"||' '||s.start_time)::TIMESTAMP AS slot_start_at, DATEADD(MIN,330,s.created_at) AS slot_created_at, 
+		DATEADD(MIN,330,s.deleted_at) AS slot_deleted_at, DATEADD(MIN,330,s.updated_at) AS slot_updated_at, 
+		COUNT(CASE WHEN b.deleted_at IS NULL THEN b.id END) AS total_bookings,
+		MAX(CASE WHEN b.deleted_at IS NULL THEN DATEADD(MIN,330,b.created_at) END) AS last_booking_at
+FROM pe_hive_slotting_hive.slots s
+LEFT JOIN pe_hive_slotting_hive.bookings b ON s.id=b.slot_id
+WHERE s.user_profile_id=41 AND s."date" BETWEEN '2019-06-01' AND '2019-06-30'
+GROUP BY 1,2,3,4,5,6,7,8,9,10
+ORDER BY slot_start_at ;
